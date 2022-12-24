d4770db18083c49c0d12d04616bd80e68a252d2c
Miracle Ikechukwu <miracleboy4gold@gmail.com>
2022-12-24 02:56:47 -0800

    Answers for 11-16

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of even fibonacci sequence up to 4,000,000
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum_of_even_fib = 0;
+	int a;
+	int b;
+	int sum = 1;
+
+	a = 1;
+	b = 1;
+
+	while (b < 4000000)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		if ((sum <= 4000000) && (sum % 2 ==0))
+			sum_of_even_fib += sum;
+	}
+	printf("%d\n", sum_of_even_fib);
+
+	return (0);
+}
